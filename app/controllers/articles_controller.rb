79718f3d1bceb4b5c4bc39d221b7489298ac58a6
asumi nakajima <nakajima0848@gmail.com>
2022-10-07 13:11:16 +0900

    croud機能の追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,33 +11,45 @@ class ArticlesController < ApplicationController
   def create 
     @article = Article.new(article_params)
     if @article.save
-      redirect_to articles_path 
+      redirect_to articles_path, notice:"投稿しました！"
     else
       render :new
     end
 
   end
 
-  # def show 
-  #   # @article = Article.find(params[:id])
-  # end
+  def show 
+    @article = Article.find(params[:id])
+  end
 
   def edit 
-
+    @article = Article.find(params[:id])
   end
 
   def update 
-
+    @article = Article.find(params[:id])
+    render :new if params[:back]
+    if @article.update(article_params)
+      redirect_to articles_path, notice:"投稿を編集しました！"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @article.destroy
+    redirect_to articles_path, notice:"投稿を編集しました！"
+  end
 
+  def confirm
+    @article = Article.new(article_params)
+    render :new if @article.invalid?
   end
 
   private
 
   def article_params
-    params.require(:article).permit(:content, :image) 
+    params.require(:article).permit(:content, :image, :image_cache) 
   end
 
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
   belongs_to :user
-  # mount_uploader :image, ImageUploader
+  validates :content, presence: true, length:{in:1..500}
+  mount_uploader :image, ImageUploader
 end
